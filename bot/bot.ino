c5053e5b86c0d00a2550924d7cc1ca85ef97af91
20jmorrison <92942539+20jmorrison@users.noreply.github.com>
2024-03-02 16:00:50 -0500

    Can use WASD to control bot over bluetooth

--- bot/bot.ino
+++ bot/bot.ino
@@ -1,5 +1,4 @@
 #include <SoftwareSerial.h>
-
 #define AIN1 12
 #define AIN2 11
 #define BIN1 10
@@ -9,7 +8,17 @@
 #define TX 3
 #define BAUD 9600
 
+enum DIRECTION {
+  FORWARD,
+  LEFT,
+  REVERSE,
+  RIGHT
+};
+
+DIRECTION Direction;
 SoftwareSerial BTSerial(RX, TX);
+unsigned char rawByte;
+
 
 void setup() {
   Serial.begin(BAUD);
@@ -28,8 +37,89 @@ void setup() {
   digitalWrite(AIN2, HIGH);
 }
 
+
 void loop() {
   if (BTSerial.available()) {
-    Serial.println(BTSerial.read());
+    Direction = getDirection();
+    drive(Direction);
+  }
+}
+
+
+DIRECTION getDirection() {
+  rawByte = BTSerial.read();
+
+  switch (rawByte) {
+    case 0xAA:
+      {
+        Serial.println("forward");
+        return FORWARD;
+        break;
+      }
+    case 0xAB:
+      {
+        Serial.println("left");
+        return LEFT;
+        break;
+      }
+    case 0xAC:
+      {
+        Serial.println("reverse");
+        return REVERSE;
+        break;
+      }
+    case 0xAD:
+      {
+        Serial.println("right");
+        return RIGHT;
+        break;
+      }
+    default:
+      {
+        Serial.println("Direction Err");
+        break;
+      }
+  }
+}
+
+
+void drive(DIRECTION direction) {
+  switch (direction) {
+    case FORWARD:
+      {
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+        break;
+      }
+    case LEFT:
+      {
+        digitalWrite(BIN1, LOW);
+        digitalWrite(BIN2, HIGH);
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        break;
+      }
+    case REVERSE:
+      {
+        digitalWrite(BIN1, HIGH);
+        digitalWrite(BIN2, LOW);
+        digitalWrite(AIN1, HIGH);
+        digitalWrite(AIN2, LOW);
+        break;
+      }
+    case RIGHT:
+      {
+        digitalWrite(BIN1, HIGH);
+        digitalWrite(BIN2, LOW);
+        digitalWrite(AIN1, LOW);
+        digitalWrite(AIN2, HIGH);
+        break;
+      }
+    default: 
+    {
+      break;
+    }
   }
 }
